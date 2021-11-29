36009bb4fa1178495a45bafa926bc397f68be6a1
muhammadsarmadjafri <71367715+muhammadsarmadjafri@users.noreply.github.com>
2021-11-30 03:37:12 +0400

    first change

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express'); 
+
+//creating app
+const app = express();
+
+// using JSON and URL Encoded middleware app.use
+(express.json()); 
+app.use(express.urlencoded({ extended: true 
+}));
+
+//send the index.html when receiving HTTP GET /
+// app.use(express.static('public'));
+// app.get('/', (req, res) => { res.sendFile('index.html', { root: __dirname });
+// });
+
+//make the app listen on port
+const port = process.argv[2] || process.env.PORT || 3000; 
+
+const server = app.listen(port, () => { 
+    console.log(`Cart app listening at http://localhost:${port}`); 
+});
+
+//handling static HTML and EJS templates
+app.use(express.static('public')); 
+//app.use('/public', express.static('public'));
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => {
+    res.render('index'); //no need for ejs extension 
+});
+
+//route for contacts in contacts.ejs file
+app.get('/contacts', (req, res) => {
+    res.render('contacts'); 
+});``
+
+//route for register in register.ejs file
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
+//route for password in login.ejs file
+app.get('/login', (req, res) => {
+    res.render('login'); 
+});
+
+//pass requests to the router middleware
+const router = require('./routes/api');
+app.use(router);
